e04746dc36d0ec8534a91cf810854d456a94ce03
amitlahav <35001635+amitlahav@users.noreply.github.com>
2018-01-01 15:08:32 +0200

    i added our project

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour 
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+	void Start () 
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () 
+    {
+		if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(5 * speed, rb.velocity.y);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.velocity = new Vector2(-5 * speed, rb.velocity.y);
+        }
+        if (Input.GetKeyUp("d"))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (Input.GetKeyUp("a"))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (Input.GetKeyDown("w") && rb.velocity.y ==  0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 3 * speed);
+        }
+	}
+    void LateUpdate
+    {
+        camera.main.transform.position.y = 1.0;
+    }
+}
