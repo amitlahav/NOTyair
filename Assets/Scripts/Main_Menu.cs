a9b762ab11b381ccdb7bbb6a7d9913859fc03c62
me <alongerby007@gmail.com>
2018-01-21 16:34:18 -0800

    added main menu
    yeet

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Main_Menu : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(Consts.PROTOTYPE);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
