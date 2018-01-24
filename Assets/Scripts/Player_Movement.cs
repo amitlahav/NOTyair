11d1c5b847b3db81e5a33c341099c57d3d2e93bb
me <alongerby007@gmail.com>
2018-01-23 19:03:38 -0800

    Animations bg and shit
    ^^^

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,6 +6,7 @@ public class Player_Movement : MonoBehaviour
 {
     const int RIGHT = 0, LEFT = 1;
     public float speed;
+    private SpriteRenderer Player_Sprite;
     private Rigidbody2D rb;
     public GameObject Rightshot;
     public Transform Rightshotspawn;
@@ -18,11 +19,13 @@ public class Player_Movement : MonoBehaviour
     private float nextfire;
     public float FireRate;
     private float moveVelocity;
+    private Animator anim;
     private int LastKey = 0;
 	void Start () 
     {
         rb = GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
+        Player_Sprite = GetComponent<SpriteRenderer>();
 	}
     void Update()
     {
@@ -50,17 +53,20 @@ public class Player_Movement : MonoBehaviour
             //rb.velocity = new Vector2(5 * speed, rb.velocity.y);
             moveVelocity = speed * 5;
             LastKey = 0;
+            Player_Sprite.flipX = false;
         }
         if (Input.GetKey("a"))
         {
             //rb.velocity = new Vector2(-5 * speed, rb.velocity.y);
             moveVelocity = speed * -5;
             LastKey = 1;
+            Player_Sprite.flipX = true;
         }
         rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
         if (Input.GetKeyDown("w") && rb.velocity.y ==  0)
         {
             rb.velocity = new Vector2(rb.velocity.x, 3 * speed);
         }
-	}
+        anim.SetFloat("Move", Input.GetAxisRaw("Horizontal"));
+    }
 }
