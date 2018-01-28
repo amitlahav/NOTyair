108c654641c862dcddf3673909c1ffdd5234be98
me <alongerby007@gmail.com>
2018-01-28 22:05:23 +0200

    animation
    kms

---
+++ Assets/Scripts/AnimationShortcut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationShortcut : MonoBehaviour {
+    private SpriteRenderer Staff;
+
+	void Start () {
+        Staff = GetComponent<SpriteRenderer>();
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetKey("d"))
+        {
+
+            Staff.flipX = false;
+        }
+        if (Input.GetKey("a"))
+        {
+           Staff.flipX = true;
+        }
+    }
+}
