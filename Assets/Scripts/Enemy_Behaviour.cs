a22d4ea30253f357963d83c08416623fef7a9347
me <alongerby007@gmail.com>
2018-03-21 20:44:11 +0200

    reeee
    reeeeeeeeeeeeeeeee

--- Assets/Scripts/Enemy_Behaviour.cs
+++ Assets/Scripts/Enemy_Behaviour.cs
@@ -2,7 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Behaviour : MonoBehaviour {
-
-	
+public class Enemy_Behaviour : MonoBehaviour
+    {
+        Rigidbody2D rb;
+        public GameObject HealthPot;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        void Update()
+        {
+            int DropChance;
+            Debug.Log(GetComponent<Health_System_Enemies>().Health);
+            if (GetComponent<Health_System_Enemies>().Health == 0)
+            {
+                DropChance = Random.Range(1, 100);
+                if (DropChance > 90)
+                {
+                    Instantiate(HealthPot, rb.transform.position, rb.transform.rotation);
+                }
+            }
+        }
 }
